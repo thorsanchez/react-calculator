d2865445a66c2fc96be4c0ea3cdb007c4c896a09
thorsanchez <ths281@hi.is>
2025-08-26 14:17:36 +0000

    laga comment i app.js

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,4 +1,5 @@
 import { ACTIONS } from "./App"
 export default function DigitButton({dispatch, digit, className}) {
     //köllum a dispatch function á onclick
-    return <button className={className} onClick={() => dispatch ( {type: ACTIONS.ADD_DIGIT, paylo
+    return <button className={className} onClick={() => dispatch ( {type: ACTIONS.ADD_DIGIT, payload: {digit}})}>{digit}</button>
+}
